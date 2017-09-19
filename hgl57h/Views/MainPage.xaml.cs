c44d30c1fb2ec1fc9e37eb8c99bb5a4a51138690
Demeter Gabor <demetergabor94@gmail.com>
2017-09-19 23:22:27 +0200

    Add project files.

---
+++ hgl57h/Views/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using hgl57h.Models;
+using hgl57h.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace hgl57h
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        
+        public static ObservableCollection<TodoItem> Todos { get; set; } = new ObservableCollection<TodoItem>()
+        
+    {
+        new TodoItem()
+        {
+             Id = 1,
+            Title = "Tejet venni",
+            Description = "Ha van tojás, hozz tizet!",
+            Priority = Priority.Normal,
+            IsDone = true,
+            Deadline = DateTimeOffset.Now + TimeSpan.FromDays(1)
+        },
+        new TodoItem()
+        {
+            Id = 2,
+            Title = "Megírni a szakdolgozatot",
+            Description = "Minimum 40 oldal, szépen kitöltve screenshotokkal",
+            Priority = Priority.High,
+            IsDone = false,
+             Deadline = new DateTime(2017, 12, 08, 12, 00, 00, 00)
+        }
+    };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+            DataContext = this;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(TodoDetailsPage), null);
+        }
+
+        private void Details_Click(object sender, ItemClickEventArgs e)
+        {
+            var clickedItem = (TodoItem)e.ClickedItem;
+            int index = clickedItem.Id;
+            Frame.Navigate(typeof(TodoDetailsPage), index - 1);
+        }
+    }
+}
---
+++ hgl57h/Views/TodoDetailsPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using hgl57h.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace hgl57h.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class TodoDetailsPage : Page
+    {
+        
+        public TodoDetailsPage()
+        {
+            this.InitializeComponent();
+
+            PriorityBox.ItemsSource = Enum.GetValues(typeof(Priority)).Cast<Priority>();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            TodoItem todoItem = new TodoItem();
+
+            todoItem.Id = MainPage.Todos.Count() + 1;
+            todoItem.IsDone = Done_CheckBox.IsChecked.GetValueOrDefault();
+            todoItem.Priority = (Priority)PriorityBox.SelectedValue;
+            todoItem.Description = Description.Text;
+            todoItem.Title = Title.Text;
+            todoItem.Deadline = DatePicker.Date;
+
+            MainPage.Todos.Add(todoItem);
+
+            Frame.Navigate(typeof(MainPage), null); 
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter != null)
+            {
+                Done_CheckBox.IsChecked = MainPage.Todos[(int)e.Parameter].IsDone;
+                Title.Text = MainPage.Todos[(int)e.Parameter].Title;
+                Description.Text = MainPage.Todos[(int)e.Parameter].Description;
+                Title.Text = MainPage.Todos[(int)e.Parameter].Title;
+                PriorityBox.SelectedItem = MainPage.Todos[(int)e.Parameter].Priority;
+                DatePicker.Date = MainPage.Todos[(int)e.Parameter].Deadline;
+            }
+        }
+    }
+
+ 
+}
